30c5f98ad873481aff9e82ee144c96b0ace4bfdd
Josef <jdhamelink0@gmail.com>
2022-04-02 16:29:56 +0200

    First crude overhead calculation
    When running with progress bar updating commented away, still gives more than a second overhead.
    This means the actual overhead of the progress bar is likely to be less than a second.

--- test.py
+++ test.py
@@ -7,16 +7,26 @@ Author: J.D. Hamelink
 """
 
 from progress import ProgressBar
-from time import sleep
+from time import sleep, time
 
 def test():
-    total_computations = 100
+    total_computations = 500
+    computation_time = 0.01
     custom_bar = ProgressBar(total_computations, bar_width=50, char='=', head='>', todo=' ',
                              spinner=True, percentage=True)
     
+    start = time()
+
+
     for _ in range(total_computations):
         custom_bar()
-        sleep(0.1)
+        sleep(computation_time)
+    
+    end = time()
+    passed = end - start
+    overhead = passed - (total_computations * computation_time)
+    print(f'{passed = :.2f} sec')
+    print(f'{overhead = :.2f} sec')
     pass
 
 if __name__ == '__main__':
